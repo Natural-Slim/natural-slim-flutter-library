33fc36dd69d1fb55e16f9117472382a906537b4c
José Manuel Dubón <56806055+J-Dubon@users.noreply.github.com>
2022-03-16 14:14:30 -0600

    NSA-193 Method to return saved token

--- lib/utils/shared_preferences.dart
+++ lib/utils/shared_preferences.dart
@@ -3,12 +3,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class UserSharedPreferences {
 
+  /// Methos to save token
   Future<bool> saveValueToken(String token) async {
     try{
       SharedPreferences prefs = await SharedPreferences.getInstance();
       var saveStatus = await prefs.setString('token', token);
       return saveStatus;
     } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Method to return saved token
+  Future<String?> returnSavedToken() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString(ApiConstants.preferencesKeyToken);
+      return token;
+    } catch (e){
       rethrow;
     }
   }
