006a7c57f898b1898576bd7164b04bb2ea8614d5
José Manuel Dubón <56806055+J-Dubon@users.noreply.github.com>
2022-03-22 17:04:13 -0600

    NSA-206 Exception documentation

--- lib/utils/helpers/exceptions_helper.dart
+++ lib/utils/helpers/exceptions_helper.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+/// Exception handling helper class
 class ExceptionsHelper{
 
-  /// Method to handle custom exceptions
+  /// Method to handle custom exceptions.
+  /// This method only covers the most common exceptions.
+  /// Any other exception that is not covered will return an exception with a default message.
   static Exception validateApiException(http.Response response) {
     var responseJson = json.decode(response.body.toString());
     switch (response.statusCode) {
@@ -13,14 +16,15 @@ class ExceptionsHelper{
         throw ApiException(responseJson['detail'], responseJson['title'], responseJson['status']);
       case 404:
         throw ApiException(responseJson['detail'], responseJson['title'], responseJson['status']);
-      case 500:
-        throw ApiException(responseJson['detail'], responseJson['title'], responseJson['status']);
       default:
         throw ApiException('An error has occured. Please try again later or contact support.');
     }
   }
 }
 
+/// Class for custom exceptions.
+/// Each exception is made up of a code [code], message [message] and title [title].
+/// Each exception should be as clear and concise as possible.
 class ApiException implements Exception {
 
   final int? _code;
@@ -34,8 +38,13 @@ class ApiException implements Exception {
     return "$_code: $_title ${_message ?? ""}";
   }
 
+  /// Error code
   int get code => _code!;
+  
+  /// Error message
   String get message => _message!;
+  
+  /// Error title
   String get title => _title!;
 
 }
