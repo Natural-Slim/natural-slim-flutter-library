a602fd783574cd30fcb9221659129e5c28c39706
José Manuel Dubón <56806055+J-Dubon@users.noreply.github.com>
2022-06-03 11:11:29 -0600

    Changed geolocation origin

--- lib/controllers/geolocation/geolocation_controller.dart
+++ lib/controllers/geolocation/geolocation_controller.dart
@@ -1,20 +1,21 @@
 import 'package:http/http.dart' as http;
+import 'package:natural_slim_flutter_library/constants/api_constants.dart';
 
 import 'package:natural_slim_flutter_library/models/geolocation/response/geolocation_response_model.dart';
 import 'package:natural_slim_flutter_library/utils/helpers/exceptions_helper.dart';
+import 'package:natural_slim_flutter_library/utils/helpers/http_header_options_helper.dart';
 
 class GeolocationController {
 
   /// Method to get current user location
-  static Future<GeoLocationResponseModel> getGeolocation() async{
+  static Future<GeoLocationResponseModel> getGeolocation(String ip) async{
     try{
       http.Response response = await http.get(
-        Uri.parse('https://geolocation-db.com/json'),
+        Uri.parse('${ApiConstants.baseUrl}/api/geolocation?Ip=$ip'),
         headers: {
           'Content-Type':'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+          'x-Time-Zone': '0',
+          'ApiKey': ApiConstants.apiKey,
         }
       );
 
