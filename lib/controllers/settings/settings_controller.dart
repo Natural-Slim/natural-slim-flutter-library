e0399b0dbe2f1002576cfc39d6f8b8c732983125
Jose Dubon <josemdubonm@gmail.com>
2022-07-20 10:01:04 -0600

    NSA-249 Created POST and PUT methods of settings

--- lib/controllers/settings/settings_controller.dart
+++ lib/controllers/settings/settings_controller.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import 'package:natural_slim_flutter_library/constants/api_constants.dart';
+import 'package:natural_slim_flutter_library/models/settings/request/user_settings_request.model.dart';
 import 'package:natural_slim_flutter_library/models/settings/response/data_app_settings_response_model.dart';
 import 'package:natural_slim_flutter_library/models/settings/response/user_settings_response_model.dart';
 import 'package:natural_slim_flutter_library/utils/helpers/exceptions_helper.dart';
@@ -61,6 +62,60 @@ class SettingsController{
       UserSettingsResponseModel parsedResponse = UserSettingsResponseModel.fromJson(jsonDecode(response.body));
       return parsedResponse;
 
+    } catch (e) {
+      rethrow;
+    }
+  }
+  
+  /// Method for create user settings
+  Future<UserSettingsResponseModel> postUserSettings(UserSettingsRequestModel userSettingsRequest) async{
+    try{
+      String? token = await HttpHeaderOptionsHelper.getValidatedToken();
+      String timeZone = HttpHeaderOptionsHelper.getTimeZoneOffset();
+
+      http.Response response = await httpRequests.post(
+        url: '${apiConstants.baseUrl}/api/settings/user-settings',
+        headers: {
+          'Content-Type':'application/json',
+          'x-Time-Zone': timeZone,
+          'Authorization':'Bearer $token'
+        },
+        body: jsonEncode(userSettingsRequest)
+      );
+
+      if(response.statusCode != 200) {
+        ExceptionsHelper.validateApiException(response);
+      }
+
+      UserSettingsResponseModel parsedResponse = UserSettingsResponseModel.fromJson(jsonDecode(response.body));
+      return parsedResponse;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  /// Method for updating user settings
+  Future<UserSettingsResponseModel> putUserSettings(UserSettingsRequestModel userSettingsRequest) async{
+    try{
+      String? token = await HttpHeaderOptionsHelper.getValidatedToken();
+      String timeZone = HttpHeaderOptionsHelper.getTimeZoneOffset();
+
+      http.Response response = await httpRequests.put(
+        url: '${apiConstants.baseUrl}/api/settings/user-settings',
+        headers: {
+          'Content-Type':'application/json',
+          'x-Time-Zone': timeZone,
+          'Authorization':'Bearer $token'
+        },
+        body: jsonEncode(userSettingsRequest)
+      );
+
+      if(response.statusCode != 200) {
+        ExceptionsHelper.validateApiException(response);
+      }
+
+      UserSettingsResponseModel parsedResponse = UserSettingsResponseModel.fromJson(jsonDecode(response.body));
+      return parsedResponse;
     } catch (e) {
       rethrow;
     }
