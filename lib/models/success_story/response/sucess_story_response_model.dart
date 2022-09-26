6ce6370ba686001cb6c976941ac54ccbc01d9802
José Manuel Dubón <56806055+J-Dubon@users.noreply.github.com>
2022-09-26 09:15:35 -0600

    NSA-331 Created method to obtain success story

--- lib/models/success_story/response/sucess_story_response_model.dart
+++ lib/models/success_story/response/sucess_story_response_model.dart
@@ -1,33 +1,27 @@
-import 'dart:convert';
-
-class SuccesStoryResponseModel {
-  SuccesStoryResponseModel({
-      this.story,
-      this.userProgramStepId,
-      this.programStepId,
-      this.photos,
+class SuccessStoryResponseModel {
+  SuccessStoryResponseModel({
+    required this.story,
+    this.userProgramStepId,
+    this.programStepId,
+    this.photos,
   });
 
-  String? story;
+  String story;
   int? userProgramStepId;
   int? programStepId;
   List<String>? photos;
 
-  SuccesStoryResponseModel succesStoryResponseModelFromJson(String str) => SuccesStoryResponseModel.fromJson(json.decode(str));
-
-  String succesStoryResponseModelToJson(SuccesStoryResponseModel data) => json.encode(data.toJson());
-
-  factory SuccesStoryResponseModel.fromJson(Map<String, dynamic> json) => SuccesStoryResponseModel(
-      story: json["story"],
-      userProgramStepId: json["userProgramStepId"],
-      programStepId: json["programStepId"],
-      photos: json["photos"] != null ? List<String>.from(json["photos"].map((x) => x)) : null,
+  factory SuccessStoryResponseModel.fromJson(Map<String, dynamic> json) => SuccessStoryResponseModel(
+    story: json["story"],
+    userProgramStepId: json["userProgramStepId"],
+    programStepId: json["programStepId"],
+    photos: json["photos"] != null ? List<String>.from(json["photos"].map((x) => x)) : null,
   );
 
   Map<String, dynamic> toJson() => {
-      "story": story,
-      "userProgramStepId": userProgramStepId,
-      "programStepId": programStepId,
-      "photos": photos != null ? List<dynamic>.from(photos!.map((x) => x)) : null,
+    "story": story,
+    "userProgramStepId": userProgramStepId,
+    "programStepId": programStepId,
+    "photos": photos != null ? List<dynamic>.from(photos!.map((x) => x)) : null,
   };
 }
