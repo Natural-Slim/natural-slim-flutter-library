b7b5484e679877a884c01f273dc2e08486fb4005
José Manuel Dubón <56806055+J-Dubon@users.noreply.github.com>
2022-09-28 08:58:16 -0600

    NSA-332 Updated User profile models

--- lib/models/authentication/requests/user_profile_information_request_model.dart
+++ lib/models/authentication/requests/user_profile_information_request_model.dart
@@ -5,41 +5,29 @@ UserProfileInformationRequestModel userProfileInformationRequestModelFromJson(St
 String userProfileInformationRequestModelToJson(UserProfileInformationRequestModel data) => json.encode(data.toJson());
 
 class UserProfileInformationRequestModel {
-    UserProfileInformationRequestModel({
-        this.currentPassword,
-        this.newPassword,
-        this.email,
-        this.phoneNumber,
-        this.firstName,
-        this.lastName,
-        this.photo,
-    });
+  UserProfileInformationRequestModel({
+    this.phoneNumber,
+    required this.firstName,
+    this.lastName,
+    this.photo,
+  });
 
-    String? currentPassword;
-    String? newPassword;
-    String? email;
-    String? phoneNumber;
-    String? firstName;
-    String? lastName;
-    String? photo;
+  String? phoneNumber;
+  String firstName;
+  String? lastName;
+  String? photo;
 
-    factory UserProfileInformationRequestModel.fromJson(Map<String, dynamic> json) => UserProfileInformationRequestModel(
-        currentPassword: json["currentPassword"],
-        newPassword: json["newPassword"],
-        email: json["email"],
-        phoneNumber: json["phoneNumber"],
-        firstName: json["firstName"],
-        lastName: json["lastName"],
-        photo: json["photo"],
-    );
+  factory UserProfileInformationRequestModel.fromJson(Map<String, dynamic> json) => UserProfileInformationRequestModel(
+    phoneNumber: json["phoneNumber"],
+    firstName: json["firstName"],
+    lastName: json["lastName"],
+    photo: json["photo"],
+  );
 
-    Map<String, dynamic> toJson() => {
-        "currentPassword": currentPassword,
-        "newPassword": newPassword,
-        "email": email,
-        "phoneNumber": phoneNumber,
-        "firstName": firstName,
-        "lastName": lastName,
-        "photo": photo,
-    };
+  Map<String, dynamic> toJson() => {
+    "phoneNumber": phoneNumber,
+    "firstName": firstName,
+    "lastName": lastName,
+    "photo": photo,
+  };
 }
