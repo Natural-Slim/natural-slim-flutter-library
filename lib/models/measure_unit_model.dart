291909620cb0dce0fdec72c72ac3d4e16c296244
MarcosZuniga <mazt.frouh@gmail.com>
2022-11-09 14:54:05 -0600

    NSA-345 Implemented measure model change

--- lib/models/measure_unit_model.dart
+++ lib/models/measure_unit_model.dart
@@ -5,18 +5,21 @@ class MeasureUnitModel {
     required this.id,
     required this.unit,
     required this.abbreviation,
+    this.conversionFactor,
     this.measureUnitType,
   });
 
   int id;
   String unit;
   String abbreviation;
+  double? conversionFactor;
   MeasureUnitType? measureUnitType;
 
   factory MeasureUnitModel.fromJson(Map<String, dynamic> json) => MeasureUnitModel(
     id: json["id"],
     unit: json["unit"],
     abbreviation: json["abbreviation"],
+    conversionFactor : double.parse(json["conversionFactor"].toString()),
     measureUnitType: json["measureUnitType"] == null ? null : MeasureUnitType.fromJson(json["measureUnitType"]),
   );
 
@@ -24,6 +27,7 @@ class MeasureUnitModel {
     "id": id,
     "unit": unit,
     "abbreviation": abbreviation,
+    "conversionFactor": conversionFactor,
     "measureUnitType": measureUnitType == null ? null : measureUnitType!.toJson(),
   };
 }
