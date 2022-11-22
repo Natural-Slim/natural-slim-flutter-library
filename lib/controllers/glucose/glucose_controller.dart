4e389ad66c2369a8ec671b89fba8be41d8fceee9
MarcosZuniga <mazt.frouh@gmail.com>
2022-11-22 16:31:44 -0600

    NSA-378 Implemented get method for glucose records

--- lib/controllers/glucose/glucose_controller.dart
+++ lib/controllers/glucose/glucose_controller.dart
@@ -5,6 +5,7 @@ import 'package:natural_slim_flutter_library/models/glucose_tracking/request/glu
 import 'package:natural_slim_flutter_library/models/glucose_tracking/response/recorded_glucose_tracking_response_model.dart';
 
 import '../../constants/api_constants.dart';
+import '../../models/glucose_tracking/response/glucose_tracking_response_model.dart';
 import '../../utils/helpers/exceptions_helper.dart';
 import '../../utils/helpers/http_header_options_helper.dart';
 import '../../utils/http_requests/http_requests.dart';
@@ -38,6 +39,34 @@ class GlucoseController {
       }
 
       RecordedGlucoseTrackingResponseModel parsedResponse = RecordedGlucoseTrackingResponseModel.fromJson(jsonDecode(response.body));
+
+      return parsedResponse;
+        
+    } catch (e){
+      rethrow;
+    }
+  }
+
+  Future<GlucoseTrackingResponseModel> getGlucoseRecords({required DateTime startDate, required DateTime endDate, int perPage = 10, required int pageNumber, required String searchParameter}) async {
+    try{
+      String? token = await HttpHeaderOptionsHelper.getValidatedToken();
+      String timeZone = HttpHeaderOptionsHelper.getTimeZoneOffset();
+
+      http.Response response = await httpRequests.get(
+        url: '${apiConstants.baseUrl}/api/user-glucoses?StartDate=$startDate&EndDate=$endDate&SearchString=$searchParameter&PerPage=$perPage&PageNumber=$pageNumber',
+        headers: {
+          'Content-Type':'application/json',
+          'x-Time-Zone': timeZone,
+          'Authorization':'Bearer $token'
+        },
+      );
+
+      // We use the code 201 because we are creating a record
+      if(response.statusCode != 200){ 
+        ExceptionsHelper.validateApiException(response);
+      }
+
+      GlucoseTrackingResponseModel parsedResponse = GlucoseTrackingResponseModel.fromJson(jsonDecode(response.body));
 
       return parsedResponse;
         
---
+++ lib/models/glucose_tracking/response/glucose_tracking_response_model.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:natural_slim_flutter_library/models/glucose_tracking/response/glucose_standard_level_model.dart';
+import 'package:natural_slim_flutter_library/models/glucose_tracking/response/paginated_glucose_date_record_model.dart';
+
+import '../../measure_unit_model.dart';
+
+class GlucoseTrackingResponseModel {
+    GlucoseTrackingResponseModel({
+        required this.start,
+        required this.end,
+        required this.count,
+        this.glucoseStandardLevelDto,
+        required this.measureUnitDto,
+        required this.paginatedRecords,
+    });
+
+    DateTime start;
+    DateTime end;
+    int count;
+    MeasureUnitModel measureUnitDto;
+    List<GlucoseStandardLevel>? glucoseStandardLevelDto;
+    PaginatedGlucoseDateRecordModel paginatedRecords;
+
+    static GlucoseTrackingResponseModel glucoseTrackingResponseModelFromJson(String str) => GlucoseTrackingResponseModel.fromJson(json.decode(str));
+
+    static String glucoseTrackingResponseModelToJson(GlucoseTrackingResponseModel data) => json.encode(data.toJson());
+
+    factory GlucoseTrackingResponseModel.fromJson(Map<String, dynamic> json) => GlucoseTrackingResponseModel(
+        start: DateTime.parse(json["start"]),
+        end: DateTime.parse(json["end"]),
+        count: json["count"],
+        measureUnitDto: MeasureUnitModel.fromJson(json["measureUnitDto"]),
+        glucoseStandardLevelDto: List<GlucoseStandardLevel>.from(json["glucoseStandardLevelDto"].map((x) => GlucoseStandardLevel.fromJson(x))),
+        paginatedRecords: PaginatedGlucoseDateRecordModel.fromJson(json["paginatedRecords"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "start": start.toIso8601String(),
+        "end": end.toIso8601String(),
+        "count": count,
+        "measureUnitDto": measureUnitDto.toJson(),
+        "glucoseStandardLevelDto": glucoseStandardLevelDto != null ? List<GlucoseStandardLevel>.from(glucoseStandardLevelDto!.map((x) => x.toJson())) : [],
+        "paginatedRecords": paginatedRecords.toJson(),
+    };
+}
