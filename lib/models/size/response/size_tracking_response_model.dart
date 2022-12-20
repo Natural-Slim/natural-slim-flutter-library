449834d163a096e61718155a28e8136d7e0b672c
MarcosZuniga <mazt.frouh@gmail.com>
2022-12-20 11:28:25 -0600

    NSA-386 Implemented empty return method for search engine consumption

--- lib/models/size/response/size_tracking_response_model.dart
+++ lib/models/size/response/size_tracking_response_model.dart
@@ -1,4 +1,5 @@
 import 'package:natural_slim_flutter_library/models/measure_unit_model.dart';
+import 'package:natural_slim_flutter_library/models/paging_info/paging_info.dart';
 import 'package:natural_slim_flutter_library/models/size/response/paginated_size_record.dart';
 
 class SizeTrackingResponseModel {
@@ -15,6 +16,29 @@ class SizeTrackingResponseModel {
     int count;
     MeasureUnitModel measureUnitDto;
     SizePaginatedRecords paginatedRecords;
+
+    static SizeTrackingResponseModel empty(){
+
+      return SizeTrackingResponseModel(
+        start: DateTime.now(), 
+        end: DateTime.now(), 
+        count: 0, 
+        measureUnitDto: MeasureUnitModel(
+          id: 0, 
+          unit: '', 
+          abbreviation: ''
+        ), 
+        paginatedRecords: SizePaginatedRecords(
+          records: [], 
+          pagingInfo: PagingInfo(
+            perPage: 0, 
+            currentPage: 0, 
+            totalRecords: 0, 
+            totalPages: 0
+          )
+        )
+      );
+    }
 
     factory SizeTrackingResponseModel.fromJson(Map<String, dynamic> json) => SizeTrackingResponseModel(
         start: DateTime.parse(json["start"]),
